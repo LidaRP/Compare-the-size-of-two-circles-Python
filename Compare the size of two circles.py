dab000cb8f4665fdfa1a4ea6fc740d2ace068b2a
LidaRP <rahmanipiyani.lida@gmail.com>
2022-05-10 14:23:00 +0430

    Compare the size of two circles

---
+++ Compare the size of two circles.py
@@ -0,0 +1,9 @@
+radius_1 = float(input('Please enter the radius of the first circle : '))
+radius_2 = float(input('Please enter the radius of the second circle : '))
+
+if radius_1 > radius_2 :
+    print('The first circle is larger than the second circle')
+else :
+    print('The second circle is larger than the first circle')
+
+    
